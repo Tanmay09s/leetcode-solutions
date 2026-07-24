49cb73efb62d5f97cad67253c35a43747aac3f2a
Tanmay09s <tanmayshahapure@gmail.com>
2026-07-24 13:05:15 +0530

    Time: 807 ms (60.67%), Space: 35.4 MB (80.9%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& arr) {
-         int n = arr.size();
         const int MAXX = 2048;
-        
-        vector<bool>xorPairs(MAXX,false);
-        vector<int>ans(MAXX,false);
+        vector<bool>seen(MAXX,false);
+        vector<bool>ans(MAXX,false);
+        int n = arr.size();
+        int cnt = 0;
 
-        for(int i=0;i<n;i++){
+        for(int i =0;i<n;i++){
             for(int j = i;j<n;j++){
-                xorPairs[arr[i] ^ arr[j]] = true;
+                seen[arr[i] ^ arr[j]] = true;
             }
         }
 
-       for(int num : arr){
-        for(int i=0;i<MAXX;i++){
-            if(xorPairs[i]){
-                ans[num ^ i] = true;
+        for(int num : arr){
+            for(int i=0;i<MAXX;i++){
+                if(seen[i]){
+                    ans[num ^ i] = true;
+                }
             }
         }
-       }
 
-       int cnt =0;
-
-       for(int i=0;i<ans.size();i++){
-        if(ans[i])cnt++;
-       }
-       return cnt;
+        for(bool i : ans){
+            if(i)cnt++;
+        }
+        return cnt;
     }
 };
