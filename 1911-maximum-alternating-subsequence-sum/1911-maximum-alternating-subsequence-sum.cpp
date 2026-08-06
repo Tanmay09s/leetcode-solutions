359f35977162c88e6327982fb5e4406ab1dd8dcc
Tanmay Rajendra Shahapure <tanmayshahapure@gmail.com>
2026-08-06 23:03:06 +0530

    Time: 320 ms (5.16%), Space: 166.6 MB (21.16%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-vector<vector<long long>>dp;
 
-    long long solve(int i,int even,vector<int>& arr){
+    long long helper(vector<int>&arr,vector<vector<long long>>&dp,int i,int even){
         if(i >= arr.size())return 0;
         if(dp[i][even] != -1)return dp[i][even];
 
-        long long skip = solve(i+1,even,arr);
-
+        long long skip = helper(arr,dp,i+1,even);
         long long take;
-        if(even)take = arr[i] + solve(i+1,0,arr);
-        else take = -arr[i] + solve(i+1,1,arr);
+        if(even)take = arr[i] + helper(arr,dp,i+1,0);
+        else take = -arr[i] + helper(arr,dp,i+1,1);
 
         return dp[i][even] = max(take,skip);
     }
     long long maxAlternatingSum(vector<int>& arr) {
         int n = arr.size();
+        vector<vector<long long>>dp;
         dp.assign(n,vector<long long>(2,-1));
-
-        return solve(0,1,arr);
+        return helper(arr,dp,0,1);
     }
 };
