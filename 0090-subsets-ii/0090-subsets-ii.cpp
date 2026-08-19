4b94d9f4c0c54cfabe7e1a76f8fb8fd80fda2774
Tanmay Rajendra Shahapure <tanmayshahapure@gmail.com>
2026-08-19 13:35:58 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.01%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    void helper(vector<vector<int>>&ans,vector<int>&nums,vector<int>&temp,int idx){
+        ans.push_back(temp);
+
+        for(int i = idx ;i<nums.size();i++){
+            if(i > idx && nums[i] == nums[i-1])continue;
+
+            temp.push_back(nums[i]);
+            helper(ans,nums,temp,i+1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        vector<int>temp;
+        helper(ans,nums,temp,0);
+
+        return ans;
+    }
+};
