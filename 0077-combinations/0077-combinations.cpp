20a10bcaea7865f11ed0f616fa7ccc732cea401a
Tanmay Rajendra Shahapure <tanmayshahapure@gmail.com>
2026-08-21 13:44:14 +0530

    Time: 35 ms (90.94%), Space: 62.4 MB (96.81%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    void helper(int start,vector<vector<int>>&ans,vector<int>&temp,int n,int k){
+        if(temp.size() == k){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = start;i<=n;i++){
+            temp.push_back(i);
+            helper(i+1,ans,temp,n,k);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        int start = 1;
+        vector<int>temp ;
+
+        helper(start,ans,temp,n,k);
+        return ans;
+    }
+};
